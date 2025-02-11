23a17a76dbdcefc4264722f098329a05e79c569a
hyunji <phj9573@naver.com>
2025-02-11 17:03:36 +0900

    first commit

---
+++ src/main/java/com/example/oliveyoungbe/OliveyoungBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.oliveyoungbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OliveyoungBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OliveyoungBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oliveyoungbe/OliveyoungBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oliveyoungbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OliveyoungBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
