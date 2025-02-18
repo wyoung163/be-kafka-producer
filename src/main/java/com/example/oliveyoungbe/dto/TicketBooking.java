222ad35941cf4722af4413e2d718e4f663d72038
seoyun0311 <yseoy0311@gmail.com>
2025-02-18 10:36:51 +0900

    Fix: 사용자 식별자 설정

--- src/main/java/com/example/oliveyoungbe/dto/TicketBooking.java
+++ src/main/java/com/example/oliveyoungbe/dto/TicketBooking.java
@@ -1,12 +1,17 @@
 package com.example.oliveyoungbe.dto;
 
+import jakarta.annotation.Nullable;
 import lombok.Data;
 
 import java.sql.Timestamp;
 
 @Data
 public class TicketBooking {
+    @Nullable
+    private String uuid;
     private String eventId;
     private String timeSlot;
+    @Nullable
+    private String region;
     private Timestamp timestamp;
 }
